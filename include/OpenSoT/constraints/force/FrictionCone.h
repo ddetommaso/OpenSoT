a3ceb61124fe5f0ea8efe722ceea4ef616431dad
Enrico Mingo <enrico.mingo@iit.it>
2015-12-03 19:01:06 +0100

    as before

--- include/OpenSoT/constraints/force/FrictionCone.h
+++ include/OpenSoT/constraints/force/FrictionCone.h
@@ -15,42 +15,46 @@
     namespace constraints {
         namespace force {
 
-
+            
             class FrictionCone: public Constraint<yarp::sig::Matrix, yarp::sig::Vector> {
             public:
                 typedef boost::shared_ptr<FrictionCone> Ptr;
             protected:
 
-
+		
         std::map<std::string, double> _mu; //Friction Coefficient associated to each contact surface
-        iDynUtils& _robot;
+		iDynUtils& _robot;
+        std::map<std::string, yarp::sig::Matrix> _world_R_surfaces;
         std::vector<std::string> _ft_in_contact;
-
+                
             public:
-
+                
         /**
                  * @brief FrictionCone
                  * @param x
                  * @param robot
                  * @param mu
-                 *
-                 * Note that we want to express the constraint locally
+                 * @param world_T_surfaces is the contact surface expressed in world frame.
+                 * Note that each transformation is associated to a ft_link_name!
                  */
                 FrictionCone(const yarp::sig::Vector& x,
                                        iDynUtils &robot,
-                                       const std::map<std::string, double>& mu);
-
-
+                                       const std::map<std::string, double>& mu,
+                                       const std::map<std::string,yarp::sig::Matrix>& world_R_surfaces);
+                
+                
                 void update(const yarp::sig::Vector &x);
 
                 void setMu(const std::map<std::string, double> mu){ _mu = mu;}
+                void setTransformWorldToSurface(std::map<std::string,yarp::sig::Matrix>& world_R_surfaces)
+                { _world_R_surfaces = world_R_surfaces;}
 
             private:
         void computeAineq();
         void computeUpperBound();
 
 
-
+                
             };
         }
     }
@@ -58,3 +62,4 @@
 
 
 #endif // FRICTIONCONE_H
+
